2760baf192e58b0d56a36a75b29f61841bef559d
Iyan SyeedMiller <Sy1486Iy0413@pusd.us>
2023-08-29 11:09:15 -0700

    skissors

--- Unit_1.py
+++ Unit_1.py
@@ -1,2 +1,15 @@
-print('\nhello worm')
+import random
+
+skissors = [('scissors',0),('paper',1),('rock',2),]
+for element in skissors:
+    space = ' ' * (10 - len(element[0]))
+    print(f'{element[0]}{space}| {element[1]}')
+
+pl_choice = input(': ')
+
+com_choice = random.choice(skissors)
+print(com_choice)
+
+if ((int(pl_choice) + 1) % 3 - com_choice[1] < 2):
+    print('yay')
 
