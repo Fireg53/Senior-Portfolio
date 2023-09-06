0efd5c6e891ac83af2fb75fed222534ade500933
Fireg53 <91575437+Fireg53@users.noreply.github.com>
2023-09-05 22:53:32 -0700

    organization

--- Unit_1.py
+++ Unit_1.py
@@ -1,24 +1,40 @@
 import random
 
+#add as many as you want! it will scale
 skissors = [('rock','🪨'),('fire','🔥'),('scissors','✂️'),('sponge','🧽'),('paper','📄'),('air','💨'),('water','💧'),]
-for i, element in enumerate(skissors):
-    space = ' ' * (10 - len(element[0]))
-    print(f'{element[0]}{space}| {i}')
 
-win_len = len(skissors) // 2
+def print_choices():
+    print()
+    for i, element in enumerate(skissors):
+        space = ' ' * (10 - len(element[0]))
+        print(f'{element[0]}{space}| {i}')
 
-while True:
-    pl_choice = int(input('~~~~~~~~~~~~~\n: ')) % len(skissors)
-    com_choice = random.randint(0, len(skissors)-1)
-    print(f"\n{skissors[pl_choice][1]}  vs {skissors[com_choice][1]}\n")
-    if pl_choice == com_choice:
-        print('Tie!')
-        continue
-    if pl_choice >= len(skissors) - win_len:
-        pl_choice = pl_choice - len(skissors)
-    if com_choice - pl_choice > 0 and  com_choice - pl_choice <= win_len:
-         print(f'Yay! {skissors[pl_choice][1]}  beats {skissors[com_choice][1]}')
-         continue
-    else:
-       print(f'Noo! {skissors[com_choice][1]}  beats {skissors[pl_choice][1]}')
-       continue
+#ugly camelCase function name
+def playGame(out_of):
+    wins = 0
+    print_choices()
+
+    win_len = len(skissors) // 2
+    while out_of > 0:
+        pl_choice = int(input('~~~~~~~~~~~~~\n: ')) % len(skissors)
+        com_choice = random.randint(0, len(skissors)-1)
+        print(f"\n{skissors[pl_choice][1]}  vs {skissors[com_choice][1]}\n")
+        if pl_choice == com_choice:
+            print('Tie!')
+            continue
+        if pl_choice >= len(skissors) - win_len:
+            pl_choice = pl_choice - len(skissors)
+        if com_choice - pl_choice > 0 and  com_choice - pl_choice <= win_len:
+            print(f'Yay! {skissors[pl_choice][1]}  beats {skissors[com_choice][1]}')
+            out_of -= 1
+            wins += 1
+        else:
+            print(f'Noo! {skissors[com_choice][1]}  beats {skissors[pl_choice][1]}')
+            out_of -= 1
+            wins -= 1
+    print()
+    if wins < 0: return "computer wins!"
+    elif wins > 0: return "player wins!"
+    else: return "tie!"
+
+print(playGame(int(input("play out of: "))))
