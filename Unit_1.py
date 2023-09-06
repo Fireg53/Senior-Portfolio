a77ab0c3424e7e730a348a147537b9b90d83ff42
Fireg53 <91575437+Fireg53@users.noreply.github.com>
2023-09-05 22:26:48 -0700

    final tweaks

--- Unit_1.py
+++ Unit_1.py
@@ -1,23 +1,24 @@
 import random
 
-skissors = [('scissors',0),('paper',1),('rock',2),('gun',3),('lazer',4),]
-for element in skissors:
+skissors = [('rock','🪨'),('fire','🔥'),('scissors','✂️'),('sponge','🧽'),('paper','📄'),('air','💨'),('water','💧'),]
+for i, element in enumerate(skissors):
     space = ' ' * (10 - len(element[0]))
-    print(f'{element[0]}{space}| {element[1]}')
+    print(f'{element[0]}{space}| {i}')
 
 win_len = len(skissors) // 2
 
-while False:
-    pl_choice = int(input(': '))
+while True:
+    pl_choice = int(input('~~~~~~~~~~~~~\n: ')) % len(skissors)
+    com_choice = random.randint(0, len(skissors)-1)
+    print(f"\n{skissors[pl_choice][1]}  vs {skissors[com_choice][1]}\n")
     if pl_choice == com_choice:
-        print('again')
-    print(pl_choice,com_choice)
+        print('Tie!')
+        continue
     if pl_choice >= len(skissors) - win_len:
         pl_choice = pl_choice - len(skissors)
     if com_choice - pl_choice > 0 and  com_choice - pl_choice <= win_len:
-         print(f'Yay! {pl_choice} beats {com_choice}')
-         break
+         print(f'Yay! {skissors[pl_choice][1]}  beats {skissors[com_choice][1]}')
+         continue
     else:
-       print(f'Noo! {com_choice} beats {pl_choice}')
-       break
-
+       print(f'Noo! {skissors[com_choice][1]}  beats {skissors[pl_choice][1]}')
+       continue
